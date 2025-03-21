58bd9874d257c0350eac9e7b68e299725c36f025
abdullokhonz <abdullokhon1206@gmail.com>
2025-03-21 12:58:42 +0500

    second commit

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,5 +1,55 @@
-import {HttpInterceptor, HttpInterceptorFn} from '@angular/common/http';
+import {HttpHandlerFn, HttpInterceptorFn, HttpRequest} from '@angular/common/http';
+import {AuthService} from './auth.service';
+import {inject} from '@angular/core';
+import {catchError, switchMap, throwError} from 'rxjs';
 
-// export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
-//
-// }
+let isRefreshing: boolean = false;
+export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService)
+  const token = authService.token
+
+  if (!token) return next(req)
+
+  if (isRefreshing) {
+    return refreshAndProceed(authService, req, next)
+  }
+
+  return next(addToken(req, token))
+    .pipe(
+      catchError(error => {
+        if (error.status === 403) {
+          return refreshAndProceed(authService, req, next)
+        }
+
+        return throwError(error);
+      })
+    )
+}
+
+const refreshAndProceed = (
+  authService: AuthService,
+  req: HttpRequest<any>,
+  next: HttpHandlerFn
+) => {
+  if (!isRefreshing) {
+    isRefreshing = true;
+
+    return authService.refreshAuthToken()
+      .pipe(
+        switchMap(res => {
+          isRefreshing = false;
+          return next(addToken(req, res.access_token))
+        })
+      )
+  }
+
+  return next(addToken(req, authService.token!))
+}
+
+const addToken = (req: HttpRequest<any>, token: string) => {
+  return req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+}
